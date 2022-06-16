0bd78550ef327ce75e607e0d4bb21b3bbb32bb96
keniossai <kenossai1@gmail.com>
2022-06-16 12:58:31 +0100

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import Header from './components/Header'
+import './style.css';
+
+export default function App() {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+}
---
+++ components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+export default function Header(){
+  return(
+    
+    <div className='nav'>
+      <img src='https://www.pngall.com/wp-content/uploads/2016/03/Meme-Vector-PNG.png'  style={{width: "100px"}}/>
+      <h2>Meme Generator</h2>
+    </div>
+  )
+}
