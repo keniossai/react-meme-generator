76c7fe9996178414af9e31a27b27067906068d84
keniossai <kenossai1@gmail.com>
2022-06-16 15:47:45 +0100

    Added memes data

--- components/Meme.jsx
+++ components/Meme.jsx
@@ -1,14 +1,24 @@
 import React from 'react'
-
+import data from '../data'
 
 export default function Meme(){
+
+  function getMemeImage(){
+    const memesArray = data.data.memes
+    const randomNumber = Math.floor(Math.random() * memesArray.length)
+    const url = memesArray[randomNumber].url
+    console.log(url)
+  }
   return(
     <main>
-      <form className="form">
+      <div className="form">
         <input className="form-input" type="text" placeholder="Top text" />
         <input className="form-input" type="text" placeholder="Bottom text" />
-          <button className='form-button'>Get a new meme image</button>
-      </form>
+          <button className='form-button' onClick={getMemeImage}>Get a new meme image</button>
+      </div>
     </main>
   )
 }
+
+
+// One thing most people fail to do is utilize the use of social media, when they see the success of others they get overwhelmed by it, and then pressure sets it.
